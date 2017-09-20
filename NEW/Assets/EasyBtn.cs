45053b5bd81f8f0b0a5cbe15859ba9997c8e87d8
Kenneth Stewart <stewa584@msu.edu>
2017-09-20 17:50:59 -0400

    Modeled LIving Room

---
+++ NEW/Assets/EasyBtn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EasyBtn : MonoBehaviour {
+
+    public void easyGameBtn(string newGameLvl)
+    {
+        SceneManager.LoadScene(newGameLvl);
+    }
+}
